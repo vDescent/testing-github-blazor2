ce0f415b550dfeb2a7ed0a3ca24ca0e66369658b
Jasmine <jasmine.wijanarko@gmail.com>
2025-04-08 10:24:50 +0700

    Commit to merge Jasmine

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e79b35ce2152cbf628754979446b59a757f05aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b4a8eb27234a621565db5da60905541146c3089")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
