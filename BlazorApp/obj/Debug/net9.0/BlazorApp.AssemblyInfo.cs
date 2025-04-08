37176ff0fdcf202440603048573de01c594b4880
vDescent <timotiusryjaska@gmail.com>
2025-04-08 09:37:33 +0700

    Test edit Home.razor

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c1815b208e9f7d8278b5893e85099ec7a917b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e79b35ce2152cbf628754979446b59a757f05aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
